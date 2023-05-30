4196bcd810d24d4bc461caf29c2a7f2e768ca284
Proundmooke <fai27sitarak@gmail.com>
2023-05-30 20:15:27 +0700

    test

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off", // Turn off requirement of 'React' being in scope for JSX
+    "no-console": "warn",
+  },
+};
