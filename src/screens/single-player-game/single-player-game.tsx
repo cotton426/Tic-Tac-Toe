8237985f4e079f4bfdffa06c5e24f408d0868b69
Proundmooke <fai27sitarak@gmail.com>
2023-06-04 03:49:52 +0700

    feat:add sound when insertCell

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,26 +1,25 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useState, useRef } from "react";
 import { SafeAreaView } from "react-native";
 import styles from "./single-player-game.styles";
 import { GradientBackground } from "@components";
 import { Board } from "@components";
 import { BoardState, isEmpty, isTerminal, getBestMove } from "@utils";
+import { Audio } from "expo-av";
 
 export default function Game(): ReactElement {
+  // prettier-ignore
   const [state, setstate] = useState<BoardState>([
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
+    null,null,null,
+    null,null,null,
+    null,null,null,
   ]);
   const [turn, setTurn] = useState<"HUMAN" | "BOT">(
     Math.random() < 0.5 ? "HUMAN" : "BOT"
   );
   const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
+
+  const popSoundRef = useRef<Audio.Sound | null>(null);
+  const pop2SoundRef = useRef<Audio.Sound | null>(null);
 
   const gameResult = isTerminal(state);
 
@@ -29,6 +28,13 @@ export default function Game(): ReactElement {
     if (stateCopy[cell] || isTerminal(stateCopy)) return;
     stateCopy[cell] = symbol;
     setstate(stateCopy);
+    try {
+      symbol === "x"
+        ? popSoundRef.current?.replayAsync()
+        : pop2SoundRef.current?.replayAsync();
+    } catch (error) {
+      console.error();
+    }
   };
 
   const handleOncellPressed = (cell: number): void => {
@@ -59,6 +65,32 @@ export default function Game(): ReactElement {
       }
     }
   }, [state, turn]);
+
+  useEffect(() => {
+    // Load sound
+    /* eslint-disable @typescript-eslint/no-var-requires */
+    const popSoundObject = new Audio.Sound();
+    const pop2SoundObject = new Audio.Sound();
+
+    const loadSounds = async () => {
+      try {
+        await popSoundObject.loadAsync(require("@assets/pop.wav"));
+        popSoundRef.current = popSoundObject;
+
+        await pop2SoundObject.loadAsync(require("@assets/pop.mp3"));
+        pop2SoundRef.current = pop2SoundObject;
+      } catch (error) {
+        console.error("Error loading sound:", error); // Log error message
+      }
+    };
+    loadSounds();
+
+    return () => {
+      // Unload sound
+      popSoundObject?.unloadAsync();
+      pop2SoundObject?.unloadAsync();
+    };
+  }, []);
 
   return (
     <GradientBackground>
