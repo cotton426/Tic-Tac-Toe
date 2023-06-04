73c0372b5ff625d91ac68718f87269be343a8ef5
Proundmooke <fai27sitarak@gmail.com>
2023-06-04 20:54:41 +0700

    feat:add animated winning-line

--- src/components/board/board-line.tsx
+++ src/components/board/board-line.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement } from "react";
-import { View, StyleSheet } from "react-native";
+import React, { ReactElement, useEffect, useRef } from "react";
+import { View, StyleSheet, Animated } from "react-native";
 import { BoardResult } from "@utils";
 
 const style = StyleSheet.create({
@@ -9,11 +9,11 @@ const style = StyleSheet.create({
   },
   vLine: {
     width: 2,
-    height: "100%",
+    // height: "100%",
   },
   hLine: {
     height: 2,
-    width: "100%",
+    // width: "100%",
   },
   dLine: {
     width: 2,
@@ -33,36 +33,65 @@ export default function BoardLine({
   gameResult,
 }: BoardLineProps): ReactElement {
   const diagonalHeight = Math.sqrt(Math.pow(size, 2) + Math.pow(size, 2));
+
+  const animationRef = useRef(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(animationRef.current, {
+      toValue: 1,
+      duration: 700,
+      useNativeDriver: false,
+    }).start();
+  }, []);
+
   return (
     <>
       {gameResult && gameResult.column && gameResult.direction === "V" && (
-        <View
+        <Animated.View
           style={[
             style.line,
             style.vLine,
-            { left: `${33.333 * gameResult.column - 16.666}%` },
+            {
+              left: `${33.333 * gameResult.column - 16.666}%`,
+              height: animationRef.current.interpolate({
+                inputRange: [0, 1],
+                outputRange: ["0%", "100%"],
+              }),
+            },
           ]}
-        ></View>
+        ></Animated.View>
       )}
       {gameResult && gameResult.row && gameResult.direction === "H" && (
-        <View
+        <Animated.View
           style={[
             style.line,
             style.hLine,
-            { top: `${33.333 * gameResult.row - 16.66}%` },
+            {
+              top: `${33.333 * gameResult.row - 16.66}%`,
+              width: animationRef.current.interpolate({
+                inputRange: [0, 1],
+                outputRange: ["0%", "100%"],
+              }),
+            },
           ]}
-        ></View>
+        ></Animated.View>
       )}
       {gameResult && gameResult.diagonal && gameResult.direction === "D" && (
-        <View
+        <Animated.View
           style={[
             style.line,
             style.dLine,
             {
-              height: diagonalHeight,
+              height: animationRef.current.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, diagonalHeight],
+              }),
               transform: [
                 {
-                  translateY: -(diagonalHeight - size) / 2,
+                  translateY: animationRef.current.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [size / 2, -(diagonalHeight - size) / 2],
+                  }),
                 },
                 {
                   rotateZ: gameResult.diagonal === "MAIN" ? "-45deg" : "45deg",
@@ -70,7 +99,7 @@ export default function BoardLine({
               ],
             },
           ]}
-        ></View>
+        ></Animated.View>
       )}
     </>
   );
